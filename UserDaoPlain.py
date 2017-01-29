c2baa1bd778875aa3b14018b3011990eb954267f
rasato <a.sato.uke@gmail.com>
2017-01-30 03:52:34 +0900

    add read config case

--- UserDaoPlain.py
+++ UserDaoPlain.py
@@ -1,21 +1,30 @@
 import pymysql.cursors
-
-connection = pymysql.connect(host='localhost',
-                             port=13306,
-                             user='userfoo',
-                             password='secret',
-                             db='mydb',
-                             charset='utf8',
-                             cursorclass=pymysql.cursors.DictCursor)
-
-with connection.cursor() as cursor:
-    print('start script')
-    sql = "SELECT id, name, age FROM users"
-    cursor.execute(sql)
-
-    result = cursor.fetchall()
-    for r in result:
-        print(r)
+import sys
+import ConfigParser
 
 
-connection.close()
+def user_dao_plain(configPath):
+    inifile = ConfigParser.SafeConfigParser()
+    inifile.read(configPath)
+
+    connection = pymysql.connect(host=inifile.get('connection', 'host'),
+                                 port=inifile.getint('connection', 'port'),
+                                 user=inifile.get('connection', 'user'),
+                                 password=inifile.get('connection', 'password'),
+                                 db='mydb',
+                                 charset='utf8',
+                                 cursorclass=pymysql.cursors.DictCursor)
+
+    with connection.cursor() as cursor:
+        print('start script')
+        sql = "SELECT id, name, age FROM users"
+        cursor.execute(sql)
+
+        result = cursor.fetchall()
+        print(result)
+
+    connection.close()
+
+
+if __name__ == '__main__':
+    user_dao_plain('./config/local.ini')
--- UserDaoPlainTest.py
+++ UserDaoPlainTest.py
@@ -1,19 +1,26 @@
 import DbAccessTestBase
 import UserDaoPlain
+import sys
+import StringIO
+
 
 class UserDaoTest(DbAccessTestBase.DbAccessTestBase):
-
     @classmethod
     def setUpClass(cls):
         super(UserDaoTest, cls).setUpClass()
 
     def setUp(self):
         self.recreateUsers()
+        self.capture = StringIO.StringIO()
+        sys.stdout = self.capture
+
+    def tearDown(self):
+        sys.stdout = sys.__stdout__
 
     def testEmpty(self):
-        UserDaoPlain
+        UserDaoPlain.user_dao_plain('./config/test.ini')
 
     def testGet(self):
         self.insertUsers([[1, 'tom', 31], [2, 'John', 25]])
-        UserDaoPlain
-
+        UserDaoPlain.user_dao_plain('./config/test.ini')
+        self.assertEqual('', self.capture.getvalue())
